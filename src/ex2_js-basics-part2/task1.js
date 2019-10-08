568e780dbe03f1ef333ca0d528d4dca05d63cd63
Hasenblake <sultanovanataliona@yandex.ru>
2019-10-08 18:49:52 +0300

    edit tasks 1, 3 for ex2

--- src/ex2_js-basics-part2/task1.js
+++ src/ex2_js-basics-part2/task1.js
@@ -1,7 +1,7 @@
 function strAndNumTypeCheck(arg){
-    if (typeof arg === "string" || typeof(arg) === "number") {
+    if (typeof arg === "string" || typeof(arg) === "number" && !isNaN(arg)) {
         return typeof arg;
-      }
+    }
     return undefined;
 }
 module.exports = strAndNumTypeCheck;
