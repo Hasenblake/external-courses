2c028a109bd554b60e898b324fe1f7a323689019
Hasenblake <sultanovanataliona@yandex.ru>
2019-10-15 21:35:00 +0300

    edit task 6 from ex3

--- src/ex3_js-objects-part1/task6.js
+++ src/ex3_js-objects-part1/task6.js
@@ -4,17 +4,7 @@ function deepCopyObject(oldObj) {
     for (let key1 in newObj) {
         if (newObj.hasOwnProperty(key1)) {
             if (typeof newObj[key1] === 'object') {
-                if (oldObj[key1] instanceof Array){
-                    newObj[key1] = Object.assign([], oldObj[key1]);
-                    for (let key2 in newObj[key1]){
-                        if (typeof oldObj[key1][key2] === 'object') {
-                            newObj[key1][key2] = deepCopyObject(oldObj[key1][key2]);
-                        }
-                    }
-                }
-                else {
                     newObj[key1] = deepCopyObject(oldObj[key1]);
-                }
             }
         }
     }
