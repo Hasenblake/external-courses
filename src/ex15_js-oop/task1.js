beb8b0219b87f144955909c05bc192f16278909a
Hasenblake <sultanovanataliona@yandex.ru>
2019-12-02 15:56:42 +0300

    fix(ex15): rename variables, add constuctors' args, add setPrototypeOf

--- src/ex15_js-oop/task1.js
+++ src/ex15_js-oop/task1.js
@@ -1,34 +1,35 @@
 let Sweet = function(name) {
     this.sweetName = name;
 };
-Sweet.prototype.getEaten = function () {
+Sweet.prototype.setZeroWeight = function () {
     this.weight = 0;
 };
 
 let Candy = function(name) {
     Sweet.apply(this, arguments);
-    this.weight = Math.round(Math.random() * (25 - 15) + 15);
+    this.weight = 20;
+    Object.setPrototypeOf(this, new Sweet);
 };
-Candy.prototype = Object.create(Sweet.prototype);
 let SweetBar = function(name) {
     Sweet.apply(this, arguments);
-    this.weight = Math.round(Math.random() * (40 - 30) + 30);
+    this.weight = 40;
+    Object.setPrototypeOf(this, new Sweet);
 };
-SweetBar.prototype = Object.create(Sweet.prototype);
 
-function SweetBox() {
-    const sweetCollection = [new Candy('Lion'), new SweetBar('Snickers'), new Candy('Dove')];
-    sweetCollection.sort((a, b) => {
-        return a.weight - b.weight;
-    });
+function SweetBox(sweets = [new Candy('Lion'), new SweetBar('Snickers'), new Candy('Dove')]) {
+    const sweetCollection = sweets;
+    sweetCollection.sort((a, b) => a.weight - b.weight);
     this.showWeight = function(){
         let weight = sweetCollection.reduce((value, boxItem) => value + boxItem.weight, 0);
+        let allWeights = "";
         console.log('Box weight: ' + weight + ' g');
-        console.log(sweetCollection[0].sweetName +': '+ sweetCollection[0].weight + ' '+
-        sweetCollection[1].sweetName +': '+ sweetCollection[1].weight +' ' +
-        sweetCollection[2].sweetName +': '+ sweetCollection[2].weight);
+        for (let sweet of sweetCollection){
+            allWeights += sweet.sweetName +': '+ sweet.weight + ', ';
+        }
+        console.log('Weights: ' + allWeights);
         return weight;
     };
+
     this.findName = function(name){
         for (let key of sweetCollection){
             if (key.sweetName === name){
@@ -39,7 +40,8 @@ function SweetBox() {
         console.log(`${name} was not found`);
         return -1;
     };
+
     this.eat = function(sweetNum){
-        sweetCollection[sweetNum].getEaten();
+        sweetCollection[sweetNum].setZeroWeight();
     };
 }
